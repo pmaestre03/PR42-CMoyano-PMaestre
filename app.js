f03cb5e75d65de1bf912cdaea2f2f26028a7df04
Pau Maestre <96410501+pmaestre03@users.noreply.github.com>
2023-10-09 19:29:15 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+// Configurar direcció ‘/’ 
+app.get('/', getHello)
+async function getHello (req, res) {
+res.send(`Hello World`)
+}
+
+// Activar el servidor
+const httpServer = app.listen(port, appListen)
+function appListen () {
+console.log(`Example app listening on: http://localhost:${port}`)
+}
+
+// Aturar el servidor correctament
+process.on('SIGTERM', shutDown);
+process.on('SIGINT', shutDown);
+function shutDown() {
+console.log('Received kill signal, shutting down gracefully');
+httpServer.close()
+process.exit(0);
+}
